4b7d76346a6a49568e53fd2917f5ebafd6557be6
Brian Zelip <bzelip@protonmail.com>
2024-02-29 06:12:02 -0500

    Improve responsive Nav component
    Create routes data source of truth for header and footer

---
+++ src/data/routes.mjs
@@ -0,0 +1,13 @@
+/**
+ * Dictionary of routes and their titles, in order of navigation apperance.
+ * This is the single source of truth for {Nav,Footer}.astro to render nav links.
+ * Update this file when new routes are added.
+ */
+export default [
+  { '/': 'Home' },
+  { '/about': 'About' },
+  { '/news': 'News' },
+  { '/hall': 'Town Hall' },
+  { '/connect': 'Connect' },
+  { '/membership': 'RIA Membership' }
+];
