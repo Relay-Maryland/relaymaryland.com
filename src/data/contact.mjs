c772309ee52833a82141ab63f442ba91d288917b
Brian Zelip <bzelip@protonmail.com>
2024-03-02 07:24:11 -0500

    Make footer more robust

---
+++ src/data/contact.mjs
@@ -0,0 +1,14 @@
+/**
+ * The single source of contact information truth to be used in markup.
+ */
+export default {
+  email: 'RelayTownHall@gmail.com',
+  phone: { Frank: '703-300-2351' },
+  address: {
+    street: 'P.O. Box 24046',
+    city: 'Relay',
+    state: { short: 'MD', long: 'Maryland' },
+    zip: '21227'
+  },
+  facebook: 'https://www.facebook.com/RelayMd/'
+};
