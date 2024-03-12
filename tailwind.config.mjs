3ff458800db0cbdbd7189acffe4da9d5bdf2c0ee
Brian Zelip <bzelip@protonmail.com>
2024-03-12 11:39:25 -0400

    Add dark mode, add instagram to contact and footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import colors from './src/colors/colors.mjs';
 
 export default {
-  content: ['./src/**/*.{astro,html,md,svg}'],
+  content: ['./src/**/*.{astro,html,md,mdx,svg}'],
   theme: {
     screens: {
       xxs: '320px',
@@ -12,7 +12,20 @@ export default {
       ...defaultTheme.screens
     },
     colors,
-    extend: {}
+    extend: {
+      backgroundImage: {
+        envelope: 'url("/src/img/envelope-outline.svg")',
+        envelopeWhite: 'url("/src/img/envelope-outline--white.svg")',
+        fb: 'url("/src/img/fa-fb.svg")',
+        fbWhite: 'url("/src/img/fa-fb--white.svg")',
+        ig: 'url("/src/img/fa-ig.svg")',
+        igWhite: 'url("/src/img/fa-ig--white.svg")',
+        location: 'url("/src/img/map-pin-outline.svg")',
+        locationWhite: 'url("/src/img/map-pin-outline--white.svg")',
+        phone: 'url("/src/img/phone-outline.svg")',
+        phoneWhite: 'url("/src/img/phone-outline--white.svg")'
+      }
+    }
   },
   plugins: []
 };
