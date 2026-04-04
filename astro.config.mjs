c6e99789f04bc573423a1708419b2b8cdcce9098
Brian Zelip <bzelip@protonmail.com>
2026-04-04 16:35:27 -0400

    Add Astro sitemap integration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-
+  site: "https://relaymaryland.com",
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
